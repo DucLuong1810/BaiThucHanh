9da675f6275bc0852886939ad7491b9677cb69fd
DucLuong1810 <2051052079luong@ou.edu.vn>
2023-02-16 15:00:12 +0700

    thu xung dot main

--- MyApp/MyApp/Form1.cs
+++ MyApp/MyApp/Form1.cs
@@ -16,5 +16,11 @@ namespace MyApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (false) return;
+
+        }
     }
 }
